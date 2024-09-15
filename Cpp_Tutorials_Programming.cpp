cb683cceb19c53069e8d4003a530f3be699258fa
Shahudullah Khan <khan.shahudullah54@gmail.com>
2024-09-15 20:23:21 +0530

    program file 1 updated

--- Cpp_Tutorials_Programming.cpp
+++ Cpp_Tutorials_Programming.cpp
@@ -3,6 +3,11 @@
 
 int main()
 {
-    std::cout << "Hello World!!!" << std::endl;
+    bool var1 = false;
+    //double var2 = 5.5;
+    std::cout << sizeof(var1) << std::endl;
+    //std::cout << sizeof(var2) << std::endl;
+    std::cout << var1 << std::endl;
+    //std::cout << var2 << std::endl;
     std::cin.get();
 }
